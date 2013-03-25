e46a3c1fc9f9032ff4c0c5d9d80b48e5a09c912f
Sean McAllister <calicalical@gmail.com>
2013-03-25 11:56:16 +0000

    Update#1

--- ChampionProfiler/Classes/keyConfig.cs
+++ ChampionProfiler/Classes/keyConfig.cs
@@ -9,33 +9,52 @@ namespace ChampionProfiler
 {
     class keyConfig
     {
-        string evtCastSpell1;
-        string evtCastSpell2;
-        string evtCastSpell3;
-        string evtCastSpell4;
-        string evtCastAvatarSpell1;
-        string evtCastAvatarSpell2;
-        string evtLevelSpell1;
-        string evtLevelSpell2;
-        string evtLevelSpell3;
-        string evtLevelSpell4;
-        string evtSelfCastSpell1;
-        string evtSelfCastSpell2;
-        string evtSelfCastSpell3;
-        string evtSelfCastSpell4;
-        string evtSmartCastSpell1;
-        string evtSmartCastSpell2;
-        string evtSmartCastSpell3;
-        string evtSmartCastSpell4;
-        string evtSmartCastAvatarSpell1;
-        string evtSmartCastAvatarSpell2;
+        public string evtCastSpell1;
+        public string evtCastSpell2;
+        public string evtCastSpell3;
+        public string evtCastSpell4;
+        public string evtCastAvatarSpell1;
+        public string evtCastAvatarSpell2;
+        public string evtLevelSpell1;
+        public string evtLevelSpell2;
+        public string evtLevelSpell3;
+        public string evtLevelSpell4;
+        public string evtSelfCastSpell1;
+        public string evtSelfCastSpell2;
+        public string evtSelfCastSpell3;
+        public string evtSelfCastSpell4;
+        public string evtSmartCastSpell1;
+        public string evtSmartCastSpell2;
+        public string evtSmartCastSpell3;
+        public string evtSmartCastSpell4;
+        public string evtSmartCastAvatarSpell1;
+        public string evtSmartCastAvatarSpell2;
 
         public void getKeysfor(string who)
         {
             XmlDocument xmlDoc = new XmlDocument(); //* create an xml document object.
-            xmlDoc.Load("settings.xml");
+            xmlDoc.Load("../../SettingsResources/settings.xml");
 
-
+            evtCastSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastSpell1").InnerText;
+            evtCastSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastSpell2").InnerText;
+            evtCastSpell3 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastSpell3").InnerText;
+            evtCastSpell4 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastSpell4").InnerText;
+            evtCastAvatarSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastAvatarSpell1").InnerText;
+            evtCastAvatarSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtCastAvatarSpell2").InnerText;
+            evtLevelSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtLevelSpell1").InnerText;
+            evtLevelSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtLevelSpell2").InnerText;
+            evtLevelSpell3 = xmlDoc.SelectSingleNode("champion/" + who + "/evtLevelSpell3").InnerText;
+            evtLevelSpell4 = xmlDoc.SelectSingleNode("champion/" + who + "/evtLevelSpell4").InnerText;
+            evtSelfCastSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSelfCastSpell1").InnerText;
+            evtSelfCastSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSelfCastSpell2").InnerText;
+            evtSelfCastSpell3 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSelfCastSpell3").InnerText;
+            evtSelfCastSpell4 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSelfCastSpell4").InnerText;
+            evtSmartCastSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastSpell1").InnerText;
+            evtSmartCastSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastSpell2").InnerText;
+            evtSmartCastSpell3 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastSpell3").InnerText;
+            evtSmartCastSpell4 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastSpell4").InnerText;
+            evtSmartCastAvatarSpell1 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastAvatarSpell1").InnerText;
+            evtSmartCastAvatarSpell2 = xmlDoc.SelectSingleNode("champion/" + who + "/evtSmartCastAvatarSpell2").InnerText;
         }
     }
 
--- ChampionProfiler/MainWindow.xaml.cs
+++ ChampionProfiler/MainWindow.xaml.cs
@@ -28,14 +28,28 @@ namespace ChampionProfiler
         private void load_Champion(object sender, RoutedEventArgs e)
         {
             //MessageBox.Show("Shit is Working");
-            this.ChampionSelect.Visibility = Visibility.Collapsed;
-            this.ChampionProfile.Visibility = Visibility.Visible;
+            this.ChampionSelect.Visibility = Visibility.Collapsed; // Hides the main window
+            this.ChampionProfile.Visibility = Visibility.Visible;  // Shows the profile window
+
+            string champName = (string)((Button)sender).Tag; // Get the Champion name from the button Tag
+            
+            string imgSrc = "/ChampionProfiler;component/Images/" + champName + "Square.png"; // Creates the image source
+            this.ChampImage.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(imgSrc, UriKind.Relative)); // Set the image
+            this.ChampLabel.Content = champName; // Set the label content of the champion name
+
+            var champKeys = new keyConfig();
+            champKeys.getKeysfor("default");
         }
 
         private void load_mainwindow(object sender, RoutedEventArgs e)
         {
             this.ChampionProfile.Visibility = Visibility.Collapsed;
             this.ChampionSelect.Visibility = Visibility.Visible;
+        }
+
+        private void change_Key(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
